d37c83e8bdae658f360c84be3b2f46c59ffcbdb4
Ethan Martin <Martin.Ethan@pennmutual.com>
2019-02-24 16:58:46 -0500

    Error messaging

--- fix.js
+++ fix.js
@@ -16,28 +16,48 @@ module.exports = (globsToWatch, verbose = false) => {
     console.error(`eslint-auto-fix is watching ${globsToWatch.join(', ')} for file changes`)
   }
 
-  chokidar.watch(globsToWatch).on('change', fix(verbose))
+  chokidar.watch(globsToWatch)
+    .on('change', fix(verbose))
 }
 
-let fix = verbose => path => {
-  if (writingTo[path] === true) {
-    return
-  }
-
-  let output = cliEngine.executeOnFiles([path]).results[0].output
-
-  if (output) { // Something needed fixin'
-    writingTo[path] = true
-
-    if (verbose) {
-      console.error(`ESLint is fixing ${path}`)
-    }
-
-    fs.writeFile(path, output, writeError => {
-      delete writingTo[path]
-      if (writeError) {
-        console.error(`Could not write to file ${path} to auto-fix it with ESLint.`)
-      }
+let errorMessages = (fatalErrorResults, path) => {
+  fatalErrorResults.forEach(result => {
+    result.messages.forEach(({message, line, column}) => {
+      console.error(`${message} on line ${line}, column ${column} of ${path}`)
     })
+  })
+}
+
+let fatalErrors = results => results.filter(
+  result => Boolean(result.messages.filter(
+    message => message.fatal
+  ).length)
+)
+
+let fix = verbose => path => {
+  if (writingTo[path] === undefined) {
+    let results = cliEngine.executeOnFiles([path]).results
+    let fatalErrorResults = fatalErrors(results)
+
+    if (fatalErrorResults.length !== 0) {
+      errorMessages(fatalErrorResults, path)
+    } else if(results[0].output) { // Something needed fixin'
+      writeOutput(path, results[0].output, verbose)
+    }
   }
+}
+
+let writeOutput = (path, output, verbose) => {
+  writingTo[path] = true
+
+  if (verbose) {
+    console.error(`ESLint is fixing ${path}`)
+  }
+
+  fs.writeFile(path, output, writeError => {
+    delete writingTo[path]
+    if (writeError) {
+      console.error(`Could not write to file ${path} to auto-fix it with ESLint.`)
+    }
+  })
 }
