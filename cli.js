adac6ffaeba8fc47927de6da5b653e37350a5417
Ethan Martin <Martin.Ethan@pennmutual.com>
2019-02-24 21:03:25 -0500

    More readme goodness

--- cli.js
+++ cli.js
@@ -2,8 +2,6 @@
 
 let argv = require('yargs')
   .usage('$0 [globs-to-watch] [options]')
-  // TODO put in positional arg
-  // with a reminder to quote the globs unless you want the shell to expand them!
   .option('verbose', {
     default: false,
     describe: 'Whether to say when a file is being fixed',
